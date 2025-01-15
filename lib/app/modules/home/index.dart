4e1ea6233dcfb12caf8933276a0b91e599e20e6b
Agus Riyanto <aguzri10@gmail.com>
2025-01-15 21:33:26 +0700

    chore: assets

--- lib/app/modules/home/index.dart
+++ lib/app/modules/home/index.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:yt_coba_plugin/app/data/local.dart';
 import 'package:yt_coba_plugin/app/models/plugin.dart';
+import 'package:yt_coba_plugin/core/constants/icons.dart';
+import 'package:yt_coba_plugin/core/constants/images.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,7 +21,27 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final plugins = LocalData.plugins();
     return Scaffold(
-      appBar: AppBar(title: const Text('Coba Plugin')),
+      appBar: AppBar(
+        title: const Text('Coba Plugin'),
+        // leading: Padding(
+        //   padding: EdgeInsets.all(8.0),
+        //   child: Image.asset(MyImages.logo),
+        // ),
+        leading: Container(
+          padding: EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(MyImages.logo),
+            ),
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(MyIcons.fingerprintLine),
+          ),
+        ],
+      ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
         child: ListView.builder(
