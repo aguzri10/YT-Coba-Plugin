0dc44c1450b483c65ab45f493a6a6ea389777c9c
Aguz Riyanto <aguzri10@gmail.com>
2025-01-18 08:12:28 +0700

    chore: assets (#5)

---
+++ lib/core/constants/icons.dart
@@ -0,0 +1,6 @@
+class MyIcons {
+  static const String cartLine = 'assets/icons/cart-line.svg';
+  static const String fingerprintLine = 'assets/icons/fingerprint-line.svg';
+  static const String searchLine = 'assets/icons/search-line.svg';
+  static const String storeLine = 'assets/icons/store-line.svg';
+}
---
+++ lib/core/constants/images.dart
@@ -0,0 +1,4 @@
+class MyImages {
+  static const flutter = 'assets/images/flutter.png';
+  static const logo = 'assets/images/logo.png';
+}
